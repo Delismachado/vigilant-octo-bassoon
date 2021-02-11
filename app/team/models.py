71d1d96d198235b43db9425dba49e82ace7f8444
Derli Machado <dam.machado1@gmail.com>
2021-02-11 15:21:56 -0300

    fix: models relationship  and include more fiels to person and team.

--- app/team/models.py
+++ app/team/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
 
-class Team(models.Model):
-    team_type = models.CharField(max_length=100, blank=False)
-    description = models.CharField(max_length=100, blank=False)
-
-    def __str__(self):
-        return f'Team: {self.description}'
-
-
 class Person(models.Model):
-    name = models.CharField(max_length=100, blank=False)    
-    teams = models.ManyToManyField(Team)
+    name = models.CharField(default='n/a', max_length=100)
+    phone = models.CharField(default='a', max_length=12)
+    age = models.IntegerField(default=0, blank=False)
 
     def __str__(self):
         return f'Person: {self.name}'
+
+
+class Team(models.Model):
+    name = models.CharField(default='n/a', max_length=100)
+    team_type = models.CharField(max_length=100, blank=False)
+    description = models.CharField(max_length=100, blank=False)
+    persons = models.ManyToManyField(Person)
+
+    def __str__(self):
+        return f'Team: {self.name}'
